a42ba7d845a439ddb0d6650e615face295268903
Ovcharenko Daniil <danila-bras@mail.com>
2020-10-15 19:34:36 +0300

    Добавлен приватный конструктор

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -6,21 +6,33 @@ using System.Threading.Tasks;
 
 namespace lab3
 {
-        partial class Bus
-        {
-            string Surname;
-            int Bus_number;
-            string Initials;
-            public string Route_number;
-            string Bus_brand;
-            int Year_started_operation;
-            int Mileage;
-            int current_year = 2020;
+    partial class Bus
+    {
+        string Surname;
+        int Bus_number;
+        string Initials;
+        public string Route_number;
+        string Bus_brand;
+        int Year_started_operation;
+        int Mileage;
+        int current_year = 2020;
 
-        
-            
 
-        }
+        string value = "            ";
+        string temp = "Hello World!";
+        const string Class = "Const";
 
+
+
+        static int Counter = 0;
+        public int Size;
+
+        //private Bus()
+        //{
+        //    Console.WriteLine("This is a private constructor!");
+        //}
        
+
+
     }
+}

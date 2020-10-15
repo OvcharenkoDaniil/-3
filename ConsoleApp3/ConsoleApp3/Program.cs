1f23612e7e18b562323b5f6dc02a77beee0ae818
Ovcharenko Daniil <danila-bras@mail.com>
2020-10-15 19:29:55 +0300

    Созданы переменные

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+        partial class Bus
+        {
+            string Surname;
+            int Bus_number;
+            string Initials;
+            public string Route_number;
+            string Bus_brand;
+            int Year_started_operation;
+            int Mileage;
+            int current_year = 2020;
+
+        
+            
+
+        }
+
+       
+    }

233a10fd82124202b66f7f6d10340af35241a12f
Ovcharenko Daniil <danila-bras@mail.com>
2020-10-15 19:58:39 +0300

    Добавлена статическая функция и функция определения кол-ва лет использования

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -6,62 +6,62 @@ using System.Threading.Tasks;
 
 namespace lab3
 {
-    partial class Bus
-    {
-        string Surname;
-        int Bus_number;
-        string Initials;
-        public string Route_number;
-        string Bus_brand;
-        int Year_started_operation;
-        int Mileage;
-        int current_year = 2020;
-
-
-        string value = "            ";
-        string temp = "Hello World!";
-        const string Class = "Const";
-
-
-
-        static int Counter = 0;
-        public int Size;
-
-        //private Bus()
-        //{
-        //    Console.WriteLine("This is a private constructor!");
-        //}
-        public string Temp
+        partial class Bus
         {
-            get
+            string Surname;
+            int Bus_number;
+            string Initials;
+            public string Route_number;
+            string Bus_brand;
+            int Year_started_operation;
+            int Mileage;
+            int current_year = 2020;
+
+        
+            string value = "            ";
+            string temp = "Hello World!";
+            const string Class = "Const";
+
+
+
+            static int Counter = 0;
+            public int Size;
+        
+            //private Bus()
+            //{
+            //    Console.WriteLine("This is a private constructor!");
+            //}
+            public string Temp
             {
-                return temp;
+                get
+                {
+                    return temp;
+                }
+                protected set
+                {
+                    temp = value;
+                }
             }
-            protected set
+            static readonly string readonly_string = "You can only read this string!";
+            static Bus()
             {
-                temp = value;
+                Console.WriteLine("This is a static constructor");
             }
-        }
 
-        static Bus()
-        {
-            Console.WriteLine("This is a static constructor");
-        }
-
-        public Bus()
-        {
+            public Bus()
+            {
             Surname = "Муругин";
             Bus_number = 1;
-            Initials = "В.А.";
+            Initials ="В.А.";
             Route_number = "111";
             Bus_brand = "МАЗ";
             Year_started_operation = 2019;
-            Mileage = 1000;
+            Mileage= 1000;
             Counter++;
-        }
+            }
 
-        public Bus(string Sur, int Bus_num, string Init, string Route_num, string Brand, int Year, int Mile)
-        {
+            public Bus(string Sur, int Bus_num, string Init, string Route_num, string Brand, int Year, int Mile)
+            {
             Surname = Sur;
             Bus_number = Bus_num;
             Initials = Init;
@@ -70,22 +70,34 @@ namespace lab3
             Year_started_operation = Year;
             Mileage = Mile;
             Counter++;
+            }
+
+            public Bus(string Sur,int Year, string Init,int Bus_num, int Mile ,string Route_num = "666", string Brand = "Mercedess")
+            {
+                Surname = Sur;
+                Bus_number = Bus_num;
+                Initials = Init;
+                Route_number = Route_num;
+                Bus_brand = Brand;
+                Year_started_operation = Year;
+                Mileage = Mile;
+                Counter++;
+            }
+        
+
+        public void operating_age(ref Bus tr, out int Operation_time)
+            {
+                Operation_time = current_year - tr.Year_started_operation;
+
+            }
+
+            static void CounterOutput()
+            {
+                Console.WriteLine("Количество созданных экземпляров объекта Bus равно {0}", Counter);
+            }
+
+
         }
 
-        public Bus(string Sur, int Year, string Init, int Bus_num, int Mile, string Route_num = "666", string Brand = "Mercedess")
-        {
-            Surname = Sur;
-            Bus_number = Bus_num;
-            Initials = Init;
-            Route_number = Route_num;
-            Bus_brand = Brand;
-            Year_started_operation = Year;
-            Mileage = Mile;
-            Counter++;
-        }
-
-        static readonly string readonly_string = "You can only read this string!";
-
-
-    }
-}
+      
+ }
